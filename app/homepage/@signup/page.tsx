59fb20e1b1a58531edba61f50c39492506b40f90
çağatay ersoy <cagatay.ersoy1@gmail.com>
2024-05-03 10:17:22 -0700

    auth work started

--- app/homepage/@signup/page.tsx
+++ app/homepage/@signup/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from "react";
 import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Checkbox, Input, Link} from "@nextui-org/react";
 import { MailIcon } from "@/app/util/MailIcon";
